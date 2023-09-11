fc4b208f8cde03b45752141f144ad13ed2588188
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-11 16:53:13 -0300

    Add mobile menu functionality with javascript.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// app/javascript/application.js
+
+import { initMobileMenu } from './mobile-menu';
+
+document.addEventListener("DOMContentLoaded", function () {
+    initMobileMenu();
+});
