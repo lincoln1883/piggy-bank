29742db9430ae544765f73c6dd52f411fe35c465
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-11 15:51:59 -0300

    Run migration to create database table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_11_183400) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_11_184903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,10 +21,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_11_183400) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "transactions", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "category_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_transactions_on_category_id"
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "transactions", "categories"
+  add_foreign_key "transactions", "users"
 end
