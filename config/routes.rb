09666b34f88dee881dac6828ca9cbed866c64a3f
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-11 18:41:48 -0300

    Reorder the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "home#index"
+
 end
