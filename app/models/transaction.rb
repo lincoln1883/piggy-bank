e72923be511847e03c65985aafefc63ea800956b
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-11 16:04:30 -0300

    Add validation.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,7 @@
 class Transaction < ApplicationRecord
+  validates_presence_of :name
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+
   belongs_to :category
   belongs_to :user
 end
