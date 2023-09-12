b358abee41c6201982bac1d4c3453fe716542ebf
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-12 01:24:00 -0300

    Redefine the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root to: "home#index"
-
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :categories
+  devise_for :users
 
+  root 'categories#index'
+  resources :categories
+  get "/", to: 'home#index'
 end
