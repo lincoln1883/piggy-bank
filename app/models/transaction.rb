68285d99f5e9d03fd9ff694e8d2620371257c90b
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-12 21:22:13 -0300

    Validate category id because the category object give error.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
   validates_presence_of :name
-  validates_presence_of :category
+  validates :category_id, presence: true
   validates_numericality_of :amount, numerical: { decimal: { greater_than: 0 } }
 
   has_and_belongs_to_many :category
