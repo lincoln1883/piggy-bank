42ba346ef0618b42d9ce7e8af2fed392d9850c4e
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-13 18:10:41 -0300

    Adjust test cases to match changes.

--- spec/models/transactions_spec.rb
+++ spec/models/transactions_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  describe 'association ' do
-    it 'belongs to user' do
-      association = described_class.reflect_on_association(:user)
-      expect(association.macro).to eq(:belongs_to)
-    end
-    it 'has many categories' do
-      association = described_class.reflect_on_association(:category)
-      expect(association.macro).to eq(:has_and_belongs_to_many)
-    end
+  it 'is valid with a name, category_id, and a positive amount' do
+    category = FactoryBot.create(:category, name: 'transit', icon: 'https://picsum.photos/200') # Create a valid category
+    transaction = FactoryBot.build(:transaction, name: 'Taxi', category_id: category.id, amount: 20)
+    expect(transaction).to be_valid
+  end
+
+  it 'is invalid without a category_id' do
+    transaction = FactoryBot.build(:transaction, category_id: nil)
+    expect(transaction).to_not be_valid
   end
 end
