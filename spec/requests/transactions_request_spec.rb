2015bf9b7bd260b68c63539dae1c1ce59ffaf476
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-13 18:14:01 -0300

    Create transactions request spec.

---
+++ spec/requests/transactions_request_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Transactions', type: :request do
+  let(:user) { create(:user) }
+  let(:categories) { create_list(:category, 3, user: user) }
+
+  before do
+    sign_in user
+  end
+
+  describe 'GET /categories/:category_id/transactions/new' do
+    it 'displays the new transaction form' do
+      get new_category_transaction_path(categories.first)
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:new)
+    end
+  end
+end
