fafc2327ca1669b6f61195d7f9217f363897674c
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-13 23:48:12 -0300

    Run new migration to remove confirmation email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # , :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { minimum: 3 }
   has_many :categories
   has_many :transactions
