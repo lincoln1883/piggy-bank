18f474ffd36bc24fec62060e956afba8cd33972f
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-14 14:01:49 -0300

    Remove hamburger menu from navbar and javascript files.

--- app/javascript/mobile-menu.js
+++ app/javascript/mobile-menu.js
@@ -1,10 +1,10 @@
-// app/javascript/mobile-menu.js
-
-export function initMobileMenu() {
-    const mobileMenuButton = document.querySelector("[data-collapse-toggle='navbar-default']");
-    const mobileMenu = document.getElementById("navbar-default");
-
-    mobileMenuButton.addEventListener("click", function () {
-        mobileMenu.classList.toggle("hidden");
-    });
-}
+// // app/javascript/mobile-menu.js
+//
+// export function initMobileMenu() {
+//     const mobileMenuButton = document.querySelector("[data-collapse-toggle='navbar-default']");
+//     const mobileMenu = document.getElementById("navbar-default");
+//
+//     mobileMenuButton.addEventListener("click", function () {
+//         mobileMenu.classList.toggle("hidden");
+//     });
+// }
